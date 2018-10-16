cbf900881b0e6eca22585885d608c1ddbed90bb6
Clayton <Claytonhart123@yahoo.com>
2018-10-16 13:47:06 -0700

    Updates

---
+++ precache-manifest.e59dc913c721c7570ee9222f94bc2896.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "40b86503b0fbe21ded5d30173ce5ad60",
+    "url": "/rooster-grin/static/media/img1.40b86503.jpg"
+  },
+  {
+    "revision": "46e7e3cc9c1b9c1e0728",
+    "url": "/rooster-grin/static/css/main.1ef5ecc9.chunk.css"
+  },
+  {
+    "revision": "1ba184a37fc02bac3192",
+    "url": "/rooster-grin/static/js/1.1ba184a3.chunk.js"
+  },
+  {
+    "revision": "a7cb5b264f6ac06291ed",
+    "url": "/rooster-grin/static/js/runtime~main.a7cb5b26.js"
+  },
+  {
+    "revision": "ac19807f11adf863cba3e97b3f5d6d83",
+    "url": "/rooster-grin/static/media/page-icon.ac19807f.svg"
+  },
+  {
+    "revision": "46c9914bb6f39ccb26f7f07396f35fdd",
+    "url": "/rooster-grin/static/media/igniter-logo-white.46c9914b.svg"
+  },
+  {
+    "revision": "93a2fadf76a2661de2112f37ad69ccb4",
+    "url": "/rooster-grin/static/media/envelope-icon.93a2fadf.svg"
+  },
+  {
+    "revision": "46e7e3cc9c1b9c1e0728",
+    "url": "/rooster-grin/static/js/main.46e7e3cc.chunk.js"
+  },
+  {
+    "revision": "ce38b545175d93bedb1b58c2405fb0d1",
+    "url": "/rooster-grin/static/media/img2.ce38b545.jpg"
+  },
+  {
+    "revision": "ec453b84afad883c68b83b0ca9a08f0c",
+    "url": "/rooster-grin/static/media/slanted-gradient-background.ec453b84.svg"
+  },
+  {
+    "revision": "ae8cb117af1866c2a28f4c2737977034",
+    "url": "/rooster-grin/static/media/hero-img.ae8cb117.jpg"
+  },
+  {
+    "revision": "0ef66ba25f850d924ebe009bad1042c6",
+    "url": "/rooster-grin/static/media/iStock-503300108.0ef66ba2.jpg"
+  },
+  {
+    "revision": "5a0e7f6d5ea57c1dc238204ed074b4cb",
+    "url": "/rooster-grin/static/media/iStock-613672992.5a0e7f6d.jpg"
+  },
+  {
+    "revision": "6a120777ccc381d2b79f2e20b4219323",
+    "url": "/rooster-grin/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c8bdf22a3042057d038eba0c00e49b9b.js"
+  "/rooster-grin/precache-manifest.e59dc913c721c7570ee9222f94bc2896.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/rooster-grin/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
